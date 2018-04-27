be43f623c797f9aafa1b4853b59c670056c50ffd
Tim Wanders <timwanders241@gmail.com>
2018-04-27 13:46:16 +0200

    Added pkgver to libsystemd and lib32-systemd

--- packages/lib32-systemd-dummy/prepare.sh
+++ packages/lib32-systemd-dummy/prepare.sh
@@ -3,4 +3,5 @@ mkdir -p files
 sudo pacman -Sw lib32-systemd --noconfirm --needed
 sudo cp /var/cache/pacman/pkg/lib32-systemd-*.pkg* .
 tar xvf lib32-systemd-*.pkg* -C files/ --force
-sudo rm -rf lib32-systemd-*.pkg*
+ls | grep lib32-systemd-*.pkg* | cut -d'-' -f3 > version
+rm -rf lib32-systemd-*.pkg*
--- packages/libsystemd-dummy/prepare.sh
+++ packages/libsystemd-dummy/prepare.sh
@@ -3,5 +3,6 @@ mkdir -p files
 sudo pacman -Sw libsystemd --noconfirm --needed
 sudo cp /var/cache/pacman/pkg/libsystemd-*.pkg* .
 tar xvf libsystemd-*.pkg* -C files/ --force
-sudo rm -rf libsystemd-*.pkg*
+ls | grep libsystemd-*.pkg* | cut -d'-' -f2 > version
+rm -rf libsystemd-*.pkg*
 export NO_INSTALL=yes
